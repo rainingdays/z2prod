fb6bb7f77de021411882f11ffdbc67858aaa36a1
rainingdays <vivalavida563@gmail.com>
2024-07-21 15:47:14 +0800

    prepare for github actions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
+use actix_web::cookie::time::format_description;
 use once_cell::sync::Lazy;
 use secrecy::ExposeSecret;
 use sqlx::Executor;
 use sqlx::{Connection, PgConnection, PgPool};
+use tracing::instrument::WithSubscriber;
 use uuid::Uuid;
 use z2prod::configuration::{self, get_configuration, DatabaseSettings};
 use z2prod::telemetry::*;
@@ -151,6 +153,33 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::ClientBuilder::new().no_proxy().build().unwrap();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}",
+            description
         );
     }
 }
